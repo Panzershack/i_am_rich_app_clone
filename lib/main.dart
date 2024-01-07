86ab2b15dbf68756af8bf6e92e83c801504099fd
pasan <pasanim@yahoo.com>
2024-01-07 12:16:57 +0530

    image assets added to Pubspec.yaml, network image changed

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ void main() {
           ),
           body: const Center(
             child: Image(
-              image: NetworkImage(
-                  'https://cdn-difln.nitrocdn.com/RhGgIzTmiXjwbtwwtHDWcsddooFiyhcI/assets/images/optimized/rev-a3b6d70/www.iconicjewelry.com/app/uploads/2017/07/red-gem-1-1024x1024.png'),
+              image: AssetImage('images/diamond.png'),
             ),
           )),
     ),
